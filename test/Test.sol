d10fd4badc70a6c9194ed2e0bb7075d3accd110c
Tomasz Slabon <tomasz.slabon@keep.network>
2021-04-15 11:56:34 +0200

    Revert "Initial commit for solhint"
    This reverts commit cab20933388fe35be9e0897b048a40937e63fc35.

--- test/Test.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.4.25;
-
-contract Test {
-    constructor() public {
-        _;
-        require(block.timestamp > 0, "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
-    }
-}
