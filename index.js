5dcdd0915dbcaab46559ce8c1029fc783dab3b2d
Tomasz Slabon <tomasz.slabon@keep.network>
2021-04-16 18:07:14 +0200

    Listed all the rules explicitly

--- index.js
+++ index.js
@@ -3,21 +3,44 @@
 // The list of rules that can be used in Solhint can be found at:
 // https://protofire.github.io/solhint/docs/rules.html
 
-// The rules below use the recommended rules from solhint with some
-// modifications
+// The rules below are from the solhint:recommended ruleset with some
+// modifications. Unfortunately, we cannot extend from solhint:recommended here
+// (when used in a project, this sharable config would lose the rules from
+// solhint:recommended), so we need to list all the rules explicitly. It looks
+// like sharable configurations don't support extending.
+
 module.exports = {
-  "extends": "solhint:recommended",
   "rules": {
-    "imports-on-top": "error",                     // recommended uses warn
-    "use-forbidden-name": "error",                 // recommended uses warn
-    "no-unused-vars": "error",                     // recommended uses warn
-    "reason-string": ["error", {"maxLength": 250}],// recommended uses warn
-    "constructor-syntax": "error",                 // recommended uses warn
-    "visibility-modifier-order": "error",          // recommended uses warn
-    "ordering": "error",                           // not used in recommended
-    "var-name-mixedcase": "off",
-    "contract-name-camelcase": "off",
-    "const-name-snakecase": "off",
-    "compiler-version": "off"
+    /* Best Practise Rules */
+    "max-states-count": ["warn", 15],
+    "no-empty-blocks": "warn",
+    "no-unused-vars": "error",
+    "payable-fallback": "warn",
+    "reason-string": ["error", {"maxLength": 250}],
+    "constructor-syntax": "error",
+    /* Style Guide Rules */
+    "quotes": ["error","double"],
+    "event-name-camelcase": "warn",
+    "func-name-mixedcase": "warn",
+    "use-forbidden-name": "error",
+    "imports-on-top": "error",
+    "ordering": "warn",
+    "visibility-modifier-order": "error",
+    /* Security Rules */
+    "avoid-call-value": "error",
+    "avoid-low-level-calls": "error",
+    "avoid-sha3": "error",
+    "avoid-suicide": "error",
+    "avoid-throw": "error",
+    "avoid-tx-origin": "error",
+    "check-send-result": "error",
+    "func-visibility": ["error", {"ignoreConstructors": false}],
+    "multiple-sends": "error",
+    "no-complex-fallback": "error",
+    "no-inline-assembly": "error",
+    "not-rely-on-block-hash": "error",
+    "not-rely-on-time": "error",
+    "reentrancy": "error",
+    "state-visibility": "error"
   }
 }
